5bd1f566e954a6709f23b52697692e82ec60bf06
Sung Youn <youn@royal-21.cs.wisc.edu>
2017-11-10 13:47:47 -0600

    multi-thread works

--- server.c
+++ server.c
@@ -21,7 +21,6 @@ pthread_mutex_t mutex;
 int head, tail;
 int *queue;
 int nbuffer;
-int connfd;
 int count;
 
 void getargs(int *port, int *nthreads, int argc, char *argv[])
@@ -40,45 +39,52 @@ void* thread_handle(void* port){
     //while loop
     //int *client = (int*) connfd;
     //printf("client fd: %d\n", client);
-    while(1){
-      while(head == tail){
+    while(1){      
+      
+      pthread_mutex_lock(&mutex);
+
+      while(count == 0){
            //printf("Head: %d, Tail: %d\n", head, tail);
            pthread_cond_wait(&empty, &mutex); 
+	printf("Count: %d, Buffer: %d\n", count, nbuffer);
            //sleep(1);
            
       //grab lock
       }
       //printf("out of while loop\n");
- 	    //pthread_mutex_lock(&mutex);
+
       //dequeue
+      int connfd = queue[head];
       queue[head] = -1;
       //printf("Debug1\n");
       head++;
       count--;
-      if(head > nbuffer)
+      if(head >= nbuffer)
           head = 0;
       //printf("Debug2\n");
       pthread_cond_signal(&fill);
       //printf("client fd: %d\n", *client);
-      requestHandle(connfd);
+      
       //printf("Debug3\n");
-      close(connfd);
+      
       //printf("Debug4\n");
-      //pthread_mutex_unlock(&mutex);
+      pthread_mutex_unlock(&mutex);
+      requestHandle(connfd);
+      close(connfd);
     }
 }
 
-void queue_put(int ptr, int value){
-    
+void queue_put(int value){
     pthread_mutex_lock(&mutex);
-    count++;
     
-    while(count > nbuffer){
+    while(count >= nbuffer){
+      
       pthread_cond_wait(&fill, &mutex);
     }
-    queue[ptr] = value;
+    queue[tail] = value;
     tail++;
-    if(tail > nbuffer)
+    count++;
+    if(tail >= nbuffer)
          tail = 0;
     //printf("Updating the queue, Tail: %d\n", tail);
     //signal to wake up the thread
@@ -87,23 +93,27 @@ void queue_put(int ptr, int value){
 }
 int main(int argc, char *argv[])
 {
-    int listenfd, port, clientlen, nthreads;
+    int listenfd, port, clientlen, nthreads, connfd;
     struct sockaddr_in clientaddr;
     getargs(&port, &nthreads, argc, argv);
     pthread_t *tids = (pthread_t*)malloc(sizeof(pthread_t)*nthreads);
-    for(int i = 0; i < nthreads; i++){
-        pthread_create(&tids[i], NULL, thread_handle, (void*) port);
-    }
-    pthread_mutex_init(&mutex, NULL);
-    queue = (int*)malloc(sizeof(int)*nbuffer);
-    listenfd = Open_listenfd(port);
+    count = 0;
     head = 0;
     tail = 0;
+    queue = (int*)malloc(sizeof(int)*nbuffer);
+    listenfd = Open_listenfd(port);
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&empty, NULL);
+    pthread_cond_init(&fill, NULL);
+    //printf("NBUFFER: %d", nbuffer);
+    for(int i = 0; i < nthreads; i++){
+        pthread_create(&tids[i], NULL, thread_handle, NULL);
+    }
     while (1) {
 	clientlen = sizeof(clientaddr);
 	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
   printf("Client %d\n", connfd);
-        queue_put(tail, connfd);
+        queue_put(connfd);
   //Getting a queue in c:
   //Array[10] First in First out. 
   //Use circular arrray to make it efficient
